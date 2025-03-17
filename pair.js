8eaf3227023b65e891d4cac0e63f583484212c66
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-03-17 22:21:07 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -73,8 +73,7 @@ _https://chat.whatsapp.com/GoXKLVJgTAAC3556FXkfFI_
 
 > 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029VagJlnG6xCSU2tS1Vz19_
  
->𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦:
- _https://www.instagram.com/xh_clinton_
+>𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/xh_clinton_
 
 
 Don't Forget To Give Star⭐ To My Repo :)`
